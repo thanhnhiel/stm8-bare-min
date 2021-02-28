61a0a13bc50bb7dbe5d3cf4ef101411352e21c00
Tran Thanh Nhi <tran.thanh.nhi@gmail.com>
2021-02-28 13:30:27 +0700

    Test LED ok

--- stm8s/examples/GPIO_blink/main.c
+++ stm8s/examples/GPIO_blink/main.c
@@ -2,14 +2,21 @@
 #include <stm8s.h>
 #include <delay.h>
 
-#define LED_PIN     4
+#define LED_PIN     3
 
 void main() {
     PD_DDR |= (1 << LED_PIN);
     PD_CR1 |= (1 << LED_PIN);
 
-    while (1) {
-        PD_ODR ^= (1 << LED_PIN);
-        delay_ms(250);
+    while (1) 
+    {
+        for (uint8_t i=0;i<3;i++)
+        {
+            PD_ODR ^= (1 << LED_PIN);
+            delay_ms(100);
+            PD_ODR ^= (1 << LED_PIN);
+            delay_ms(100);
+        }
+        delay_ms(1000);
     }
 }
