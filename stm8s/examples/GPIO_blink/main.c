60e561ab6992701fdba9206431212f5513f3ad37
Tran Thanh Nhi <tran.thanh.nhi@gmail.com>
2021-06-18 21:25:57 +0700

    Test Board stm8s mini - stm8s103f3

--- stm8s/examples/GPIO_blink/main.c
+++ stm8s/examples/GPIO_blink/main.c
@@ -2,19 +2,21 @@
 #include <stm8s.h>
 #include <delay.h>
 
-#define LED_PIN     3
+#define LED_PIN     5
+//#define LED_PIN     8
 
 void main() {
-    PD_DDR |= (1 << LED_PIN);
-    PD_CR1 |= (1 << LED_PIN);
+    PB_DDR |= (1 << LED_PIN);
+    PB_CR1 |= (1 << LED_PIN);
 
+    PB_ODR ^= (1 << LED_PIN);
     while (1) 
     {
         for (uint8_t i=0;i<3;i++)
         {
-            PD_ODR ^= (1 << LED_PIN);
+            PB_ODR ^= (1 << LED_PIN);
             delay_ms(100);
-            PD_ODR ^= (1 << LED_PIN);
+            PB_ODR ^= (1 << LED_PIN);
             delay_ms(100);
         }
         delay_ms(1000);
