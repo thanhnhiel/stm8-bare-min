fbfa3786af4645fda6c6d87d58bd4f92b8e6b57f
Tran Thanh Nhi <tran.thanh.nhi@gmail.com>
2021-07-04 14:04:01 +0700

    Add Sleep mode & Power Pin for EM4100

--- stm8l/examples/EM4100_STM8/main.c
+++ stm8l/examples/EM4100_STM8/main.c
@@ -6,15 +6,30 @@
 #include "delay.h"
 //#include "beep.h"
 
+void RTC_WakeUpClockConfig(uint8_t RTC_WakeUpClock);
+void RTC_ITConfig(uint8_t RTC_IT, uint8_t NewState);
+void RTC_SetWakeUpCounter(uint16_t RTC_WakeupCounter);
+void RTC_WakeUpCmd(uint8_t NewState);
+
 #define LED_PIN     1
 #define LED2_PIN     7
 #define LED3_PIN     2
 
+#define PWR_PIN     6
+
+#define PWR_INIT() { \
+       PB_DDR |= (1 << PWR_PIN); \
+       PB_CR1 |= (1 << PWR_PIN); \
+       }
+
+#define PWR_OFF()  PB_ODR &= ~(1 << PWR_PIN)
+#define PWR_ON()  PB_ODR |= (1 << PWR_PIN)
+#define PWR_TOGGLE()  PB_ODR ^= (1 << PWR_PIN)       
+
+
 #define LED_INIT() { \
        PC_DDR |= (1 << LED_PIN); \
        PC_CR1 |= (1 << LED_PIN); \
-       PB_DDR |= (1 << LED2_PIN | 1 << LED3_PIN); \
-       PB_CR1 |= (1 << LED2_PIN | 1 << LED3_PIN); \
        }
 
 #define LED2_INIT() { \
@@ -67,45 +82,77 @@ uint8_t NumOfBits = 0;
 
 void main() 
 {
+    uint16_t j;
     /* HSI clock prescaler */
     CLK_CKDIVR = CLK_SYSCLKDiv_8;
+    rtc_init();
+
     LED_INIT();
     LED_ON();
     LED2_INIT();
     LED3_INIT();
+    PWR_INIT();
   //  LED2_ON();
   //  LED3_OFF();
+    PWR_OFF();
     delay_ms(2000);
     UART_LowLevel_Init();
     uart_init();
     printf("Test\r\n");
    // LED2_OFF();
     LED_OFF();
+    PWR_ON();
 
    tim2_init();
    tim3Input_init();
-
+    
     enableInterrupts();
 
 
     while (1) 
     {
-       // TIM2_BKR |= TIM_BKR_MOE;
-      //  delay_ms(30);
-        if (flag!=0)
+        TIM2_BKR |= TIM_BKR_MOE;
+        PWR_ON();
+        for (j=0;j<200;j++)      // 0.59 = 200 
         {
-            for (uint8_t i=0;i<8;i++)
+            if (flag!=0)
             {
-                uart_write(buf[i]);
+                for (uint8_t i=0;i<8;i++)
+                {
+                    uart_write(buf[i]);
+                }
+                flag = 0;
             }
-            flag = 0;
+            delay_ms(1);
         }
 
-      //  TIM2_BKR &= (uint8_t)(~TIM_BKR_MOE);
-     //   delay_ms(1000);
-        
-        //delay_ms(2000);
+        PWR_OFF();
+        TIM2_BKR &= (uint8_t)(~TIM_BKR_MOE);
+        /* RTC will wake-up from halt every 3*2 second*/
+        RTC_SetWakeUpCounter(3);
+        RTC_WakeUpCmd(ENABLE);
+        /* Enter Halt mode*/
+        halt();
+        RTC_WakeUpCmd(DISABLE);
     }
+}
+
+INTERRUPT_HANDLER(RTC_IRQHandler, RTC_ISR)
+{
+    /* RTC_ClearITPendingBit */
+    RTC_ISR2 = (uint8_t)~(uint8_t)((uint16_t)RTC_IT_WUT >> 4);
+}
+
+void rtc_init()
+{
+     /* CLK_RTCClockConfig use LSI */
+    CLK_CRTCR = (uint8_t)((uint8_t)CLK_RTCCLKSource_LSI | (uint8_t)CLK_RTCCLKDiv_1);
+    /* Enable RTC clock */
+    CLK_PCKENR2 |= (uint8_t)((uint8_t)1 << 2);
+    /* RTC_WakeUpClockConfig */
+    RTC_WakeUpClockConfig(RTC_WakeUpClock_CK_SPRE_16bits);
+    /* Enable RTC WUT */
+    RTC_ITConfig(RTC_IT_WUT, ENABLE);
 }
 
 void UART_LowLevel_Init(void)
@@ -600,4 +647,104 @@ void tim2_init()
 
     /* TIM2_Ctrl PWM Outputs */
     TIM2_BKR |= TIM_BKR_MOE;
+}
+
+
+void RTC_WakeUpClockConfig(uint8_t RTC_WakeUpClock)
+{
+  /* Disable the write protection for RTC registers */
+  RTC_WPR = 0xCA;
+  RTC_WPR = 0x53;
+
+  /* Disable the Wakeup timer in RTC_CR2 register */
+  RTC_CR2 &= (uint8_t)~0x04;
+
+  /* Clear the Wakeup Timer clock source bits in CR1 register */
+  RTC_CR1 &= (uint8_t)~0x07;
+
+  /* Configure the clock source */
+  RTC_CR1 |= (uint8_t)RTC_WakeUpClock;
+
+  /* Enable the write protection for RTC registers */
+  RTC_WPR = 0xFF; 
+}
+
+void RTC_ITConfig(uint8_t RTC_IT, uint8_t NewState)
+{
+  /* Disable the write protection for RTC registers */
+  RTC_WPR = 0xCA;
+  RTC_WPR = 0x53;
+
+  if (NewState != 0)
+  {
+    /* Enable the Interrupts */
+    RTC_CR2 |= (uint8_t)((uint8_t)RTC_IT & (uint8_t)0xF0);
+    RTC_TCR1 |= (uint8_t)((uint8_t)RTC_IT & 0x01);
+  }
+  else
+  {
+    /* Disable the Interrupts */
+    RTC_CR2  &= (uint8_t)~(uint8_t)((uint8_t)RTC_IT & (uint8_t)0x00F0);
+    RTC_TCR1 &= (uint8_t)~(uint8_t)((uint8_t)RTC_IT & (uint8_t)0x01);
+  }
+
+  /* Enable the write protection for RTC registers */
+  RTC_WPR = 0xFF; 
+}
+
+void RTC_SetWakeUpCounter(uint16_t RTC_WakeupCounter)
+{
+  /* Disable the write protection for RTC registers */
+  RTC_WPR = 0xCA;
+  RTC_WPR = 0x53;
+
+
+  /* Configure the Wakeup Timer counter */
+  RTC_WUTRH = (uint8_t)(RTC_WakeupCounter >> 8);
+  RTC_WUTRL = (uint8_t)(RTC_WakeupCounter);
+
+  /* Enable the write protection for RTC registers */
+  RTC_WPR = 0xFF; 
+}
+
+void RTC_WakeUpCmd(uint8_t NewState)
+{
+  //ErrorStatus status = ERROR;
+  uint16_t wutwfcount = 0;
+
+  /* Disable the write protection for RTC registers */
+  RTC_WPR = 0xCA;
+  RTC_WPR = 0x53;
+
+  if (NewState != 0)
+  {
+    /* Enable the Wakeup Timer */
+    RTC_CR2 |= (uint8_t)(1<<RTC_CR2_WUTE);
+
+    //status = SUCCESS;
+  }
+  else
+  {
+    /* Disable the Wakeup Timer */
+    RTC_CR2 &= (uint8_t)~(1<<RTC_CR2_WUTE);
+
+    /* Wait until WUTWF flag is set */
+    while (((RTC_ISR1 & 1<<RTC_ISR1_WUTWF) == 0) && ( wutwfcount != WUTWF_TIMEOUT))
+    {
+      wutwfcount++;
+    }
+
+    /* Check WUTWF flag is set or not */
+    if ((RTC_ISR1 & 1<<RTC_ISR1_WUTWF) == 0)
+    {
+      //status = ERROR;
+    }
+    else
+    {
+      //status = SUCCESS;
+    }
+  }
+
+  /* Enable the write protection for RTC registers */
+  RTC_WPR = 0xFF; 
 }
